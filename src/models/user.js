8b68dad3a884a5a2c54a4cec02bfc2b2ebe7aaf0
DanielDCM21 <danieldcm21@outlook.com>
2020-08-01 22:53:01 -0500

    NewForm

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt-nodejs');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-    //name: String,
+    name: String,
     email: String,
     password: String
 });
