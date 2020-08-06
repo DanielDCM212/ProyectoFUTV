99d122702f41441e9f3c3bd09f5a3f9187f85fc3
DanielDCM21 <danieldcm21@outlook.com>
2020-08-05 20:08:25 -0500

    final

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
     mongodb: {
-        // URI: 'mongodb://localhost:27017/futv'
-        URI: 'mongodb+srv://DanielDCM21:Desyap123@cluster0.1eweb.mongodb.net/futv?retryWrites=true&w=majority'
+        URI: 'mongodb://localhost:27017/futv'
     }
 }
